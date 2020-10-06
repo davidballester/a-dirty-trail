197ce34dd9ea232d187cad5f1591a4b753da965c
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-06 21:29:32 +0200

    feat: merge ammunition items

--- src/mechanics/inventory.ts
+++ src/mechanics/inventory.ts
@@ -1,6 +1,37 @@
-import { Inventory } from '../models';
+import { Ammunition, Inventory } from '../models';
 
 export const takeItems = (source: Inventory, target: Inventory) => {
     target.items = [...target.items, ...source.items];
     source.items = [];
+    mergeAmmunitions(target);
+    target.items = target.items.filter(
+        (item) => !(item instanceof Ammunition) || !item.isSpent()
+    );
+};
+
+const mergeAmmunitions = (inventory: Inventory) => {
+    const ammunitionsByName: {
+        [ammunitionName: string]: Ammunition[];
+    } = inventory.items
+        .filter((item) => item instanceof Ammunition)
+        .reduce(
+            (map, ammunition) => ({
+                ...map,
+                [ammunition.name]: [
+                    ...(map[ammunition.name] || []),
+                    ammunition,
+                ],
+            }),
+            {}
+        );
+    Object.keys(ammunitionsByName).forEach((ammunitionName) => {
+        const ammunitions = ammunitionsByName[ammunitionName];
+        if (ammunitions.length > 1) {
+            const firstAmmunition = ammunitions[0];
+            ammunitions.slice(1).forEach((ammunition) => {
+                firstAmmunition.modifyAmmunition(ammunition.quantity);
+                ammunition.modifyAmmunition(-ammunition.quantity);
+            });
+        }
+    });
 };

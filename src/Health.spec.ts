f29d77903cee8157480b2fcfd1825465b9e35fd2
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-21 21:41:42 +0200

    feat: redo health

---
+++ src/Health.spec.ts
@@ -0,0 +1,79 @@
+import Health from './Health';
+
+describe('Health', () => {
+    it('initializes without error', () => {
+        new Health({ current: 0, max: 1 });
+    });
+
+    it('fails if the current is lower than 0', () => {
+        try {
+            new Health({ current: -1, max: 0 });
+            fail('error expected');
+        } catch (err) {}
+    });
+
+    it('fails if the max is lower than 0', () => {
+        try {
+            new Health({ current: 0, max: -1 });
+            fail('error expected');
+        } catch (err) {}
+    });
+
+    it('fails if the max is lower than the current', () => {
+        try {
+            new Health({ current: 2, max: 1 });
+            fail('error expected');
+        } catch (err) {}
+    });
+
+    it('gets the current', () => {
+        const health = new Health({ current: 1, max: 2 });
+        const current = health.getCurrent();
+        expect(current).toEqual(1);
+    });
+
+    it('gets the max', () => {
+        const health = new Health({ current: 1, max: 2 });
+        const max = health.getMax();
+        expect(max).toEqual(2);
+    });
+
+    describe('isAlive', () => {
+        it('is alive if current is greater than 0', () => {
+            const health = new Health({ current: 1, max: 1 });
+            const isAlive = health.isAlive();
+            expect(isAlive).toBeTruthy();
+        });
+
+        it('is not alive if current is 0', () => {
+            const health = new Health({ current: 0, max: 1 });
+            const isAlive = health.isAlive();
+            expect(isAlive).toBeFalsy();
+        });
+    });
+
+    describe('modify', () => {
+        let health: Health;
+        beforeEach(() => {
+            health = new Health({ current: 5, max: 10 });
+        });
+
+        it('modifies the health within the range', () => {
+            health.modify(3);
+            const current = health.getCurrent();
+            expect(current).toEqual(8);
+        });
+
+        it('does not exceed the max', () => {
+            health.modify(10);
+            const current = health.getCurrent();
+            expect(current).toEqual(10);
+        });
+
+        it('does not goes below 0', () => {
+            health.modify(-10);
+            const current = health.getCurrent();
+            expect(current).toEqual(0);
+        });
+    });
+});
---
+++ src/Health.ts
@@ -0,0 +1,41 @@
+class Health {
+    private current: number;
+    private max: number;
+
+    constructor({ current, max }: { current: number; max: number }) {
+        if (!Health.isValidRange(current, max)) {
+            throw new Error('invalid range');
+        }
+        this.current = current;
+        this.max = max;
+    }
+
+    getCurrent() {
+        return this.current;
+    }
+
+    getMax() {
+        return this.max;
+    }
+
+    isAlive() {
+        return this.current > 0;
+    }
+
+    modify(delta: number) {
+        this.current += delta;
+        this.current = Math.min(this.current, this.max);
+        this.current = Math.max(this.current, 0);
+    }
+
+    private static isValidRange(current, max) {
+        const isCurrentPositive = current >= 0;
+        const isMaxPositive = max >= 0;
+        const isMaxHigherOrEqualToCurrent = max >= current;
+        return (
+            isCurrentPositive && isMaxPositive && isMaxHigherOrEqualToCurrent
+        );
+    }
+}
+
+export default Health;

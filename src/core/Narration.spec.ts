b19d50ba6d0eb6cf5039ee1562c99491b52a0600
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-23 22:00:39 +0200

    feat: add narration

---
+++ src/core/Narration.spec.ts
@@ -0,0 +1,41 @@
+import Narration from './Narration';
+import Scene from './Scene';
+
+describe('Narration', () => {
+    class CustomNarration extends Narration {
+        loadNextScene(scene: Scene): void {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    it('initializes without errors', () => {
+        new CustomNarration({ title: 'My narration' });
+    });
+
+    describe('getTitle', () => {
+        it('returns the title', () => {
+            const narration = new CustomNarration({ title: 'My narration' });
+            const title = narration.getTitle();
+            expect(title).toEqual('My narration');
+        });
+    });
+
+    describe('getCurrentScene | setCurrentScene', () => {
+        let narration: Narration;
+        beforeEach(() => {
+            narration = new CustomNarration({ title: 'My narration' });
+        });
+
+        it('returns undefined', () => {
+            const currentScene = narration.getCurrentScene();
+            expect(currentScene).toBeUndefined();
+        });
+
+        it('retunrs the scene just set', () => {
+            const scene = ({ id: 'foo' } as unknown) as Scene;
+            narration.setCurrentScene(scene);
+            const returnedScene = narration.getCurrentScene();
+            expect(returnedScene).toEqual(scene);
+        });
+    });
+});
---
+++ src/core/Narration.ts
@@ -0,0 +1,26 @@
+import Scene from './Scene';
+
+abstract class Narration {
+    private title: string;
+    private currentScene?: Scene;
+
+    constructor({ title }: { title: string }) {
+        this.title = title;
+    }
+
+    getTitle(): string {
+        return this.title;
+    }
+
+    getCurrentScene(): Scene | undefined {
+        return this.currentScene;
+    }
+
+    setCurrentScene(scene: Scene) {
+        this.currentScene = scene;
+    }
+
+    abstract loadNextScene(scene: Scene): void;
+}
+
+export default Narration;

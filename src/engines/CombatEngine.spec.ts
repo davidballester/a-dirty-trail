3e5126df95893cf67485197c1a4374c5d10d127f
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-25 12:42:55 +0100

    feat: add combat engine

---
+++ src/engines/CombatEngine.spec.ts
@@ -0,0 +1,317 @@
+import { when } from 'jest-when';
+import AttackAction from '../actions/AttackAction';
+import Actor from '../core/Actor';
+import NonPlayableActor from '../core/NonPlayableActor';
+import Scene from '../core/Scene';
+import { AttackOutcome } from '../core/Weapon';
+import CombatEngine from './CombatEngine';
+import ActionBuilder from '../actions/ActionBuilder';
+import ActionsMap from '../core/ActionsMap';
+jest.mock('../actions/ActionBuilder');
+
+describe('CombatEngine', () => {
+    let combatEngine: CombatEngine;
+    let scene: Scene;
+    let sceneGetAliveActors: jest.SpyInstance;
+    let sceneContainsActor: jest.SpyInstance;
+    let janeDoe: Actor;
+    let janeDoeAction: AttackAction;
+    let janeDoeActionOutcome: AttackOutcome;
+    let janeDoeActionExecute: jest.SpyInstance;
+    let janeDoeActionCanExecute: jest.SpyInstance;
+    let johnDoe: NonPlayableActor;
+    let johnDoeIsAlive: jest.SpyInstance;
+    let johnDoeAction: AttackAction;
+    let johnDoeActionCanExecute: jest.SpyInstance;
+    let johnDoeActionOutcome: AttackOutcome;
+    let jillBloggsIsAlive: jest.SpyInstance;
+    let jillBloggs: NonPlayableActor;
+    let buildActions: jest.SpyInstance;
+    let janeDoeActions: ActionsMap;
+
+    const initializeJaneDoe = () => {
+        janeDoeActionCanExecute = jest.fn().mockReturnValue(true);
+        janeDoeActionOutcome = {} as AttackOutcome;
+        janeDoeActionExecute = jest.fn().mockReturnValue(janeDoeActionOutcome);
+        janeDoeAction = ({
+            canExecute: janeDoeActionCanExecute,
+            execute: janeDoeActionExecute,
+        } as unknown) as AttackAction;
+        const janeDoeIsAlive = jest.fn().mockReturnValue(true);
+        const janeDoeEquals = jest.fn();
+        janeDoe = ({
+            id: 'jane doe',
+            isAlive: janeDoeIsAlive,
+            equals: janeDoeEquals,
+        } as unknown) as Actor;
+        when(janeDoeEquals)
+            .mockReturnValue(false)
+            .calledWith(janeDoe)
+            .mockReturnValue(true);
+    };
+
+    const initializeJohnDoe = () => {
+        johnDoeActionOutcome = {} as AttackOutcome;
+        johnDoeActionCanExecute = jest.fn().mockReturnValue(true);
+        const johnDoeActionExecute = jest
+            .fn()
+            .mockReturnValue(johnDoeActionOutcome);
+        johnDoeAction = ({
+            canExecute: johnDoeActionCanExecute,
+            execute: johnDoeActionExecute,
+        } as unknown) as AttackAction;
+        johnDoeIsAlive = jest.fn().mockReturnValue(true);
+        const johnDoeGetNextAction = jest.fn().mockReturnValue(johnDoeAction);
+        const johnDoeEquals = jest.fn();
+        johnDoe = ({
+            id: 'john doe',
+            isAlive: johnDoeIsAlive,
+            getNextAction: johnDoeGetNextAction,
+            equals: johnDoeEquals,
+        } as unknown) as NonPlayableActor;
+        when(johnDoeEquals)
+            .mockReturnValue(false)
+            .calledWith(johnDoe)
+            .mockReturnValue(true);
+    };
+
+    const initializeJillBloggs = () => {
+        jillBloggsIsAlive = jest.fn().mockReturnValue(true);
+        const jillBloggsEquals = jest.fn();
+        jillBloggs = ({
+            id: 'jill bloggs',
+            isAlive: jillBloggsIsAlive,
+            equals: jillBloggsEquals,
+        } as unknown) as NonPlayableActor;
+        when(jillBloggsEquals)
+            .mockReturnValue(false)
+            .calledWith(jillBloggs)
+            .mockReturnValue(true);
+    };
+
+    const initializeScene = () => {
+        sceneGetAliveActors = jest.fn().mockReturnValue([johnDoe, jillBloggs]);
+        const getPlayer = jest.fn().mockReturnValue(janeDoe);
+        sceneContainsActor = jest.fn().mockReturnValue(true);
+        scene = ({
+            getAliveActors: sceneGetAliveActors,
+            getPlayer,
+            containsActor: sceneContainsActor,
+        } as unknown) as Scene;
+    };
+
+    const initializeActionBuilder = () => {
+        const actionBuilderMock = (ActionBuilder as unknown) as jest.Mock;
+        janeDoeActions = ({
+            id: 'jane doe actions',
+        } as unknown) as ActionsMap;
+        buildActions = jest.fn().mockReturnValue(janeDoeActions);
+        actionBuilderMock.mockImplementation(() => ({
+            buildActions,
+        }));
+    };
+
+    beforeEach(() => {
+        initializeJaneDoe();
+        initializeJohnDoe();
+        initializeJillBloggs();
+        initializeScene();
+        initializeActionBuilder();
+        combatEngine = new CombatEngine({ scene });
+    });
+
+    describe('getActorCurrentTurn', () => {
+        it('returns the player', () => {
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(janeDoe);
+        });
+
+        it('is the second oponent actor after advancing turn', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(johnDoe);
+        });
+
+        it('is the player oponent actor after advancing turn twice', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            combatEngine.executeNextOponentAction();
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(janeDoe);
+        });
+
+        it('is the second oponent actor after advancing turn three times', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            combatEngine.executeNextOponentAction();
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(jillBloggs);
+        });
+    });
+
+    describe('getOponentsInActionOrder', () => {
+        it('returns the oponents in the order specified on the scene', () => {
+            const oponentsInActionOrder = combatEngine.getOponentsInActionOrder();
+            expect(oponentsInActionOrder).toEqual([johnDoe, jillBloggs]);
+        });
+    });
+
+    describe('executePlayerAction', () => {
+        it('executes the action', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            expect(janeDoeActionExecute).toHaveBeenCalled();
+        });
+
+        it('returns the action outcome', () => {
+            const outcome = combatEngine.executePlayerAction(janeDoeAction);
+            expect(outcome).toEqual(janeDoeActionOutcome);
+        });
+
+        it('throws an error if the action cannot be executed', () => {
+            janeDoeActionCanExecute.mockReturnValue(false);
+            try {
+                combatEngine.executePlayerAction(janeDoeAction);
+                fail('expected an error');
+            } catch (err) {}
+            expect(janeDoeActionExecute).not.toHaveBeenCalled();
+        });
+
+        it('throws an error if it is not the player turn', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            janeDoeActionExecute.mockClear();
+            try {
+                combatEngine.executePlayerAction(janeDoeAction);
+                fail('expected an error');
+            } catch (err) {}
+            expect(janeDoeActionExecute).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('executeNextOponentAction', () => {
+        it('throws an error if it is not the oponent turn', () => {
+            try {
+                combatEngine.executeNextOponentAction();
+                fail('expected an error');
+            } catch (err) {}
+        });
+
+        it('returns undefined if the action cannot be executed', () => {
+            johnDoeActionCanExecute.mockReturnValue(false);
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actionAndOutcome = combatEngine.executeNextOponentAction();
+            expect(actionAndOutcome).toBeUndefined();
+        });
+
+        it('returns the action and the outcome', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actionAndOutcome = combatEngine.executeNextOponentAction();
+            expect(actionAndOutcome).toEqual([
+                johnDoeAction,
+                johnDoeActionOutcome,
+            ]);
+        });
+    });
+
+    describe('next oponent is killed', () => {
+        beforeEach(() => {
+            sceneGetAliveActors.mockReturnValue([jillBloggs]);
+            johnDoeIsAlive.mockReturnValue(false);
+        });
+
+        it('is the player turn again', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(janeDoe);
+        });
+
+        it('is the second oponent turn after the player', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(jillBloggs);
+        });
+
+        it('updates the list of oponents in order', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const oponentsInActionOrder = combatEngine.getOponentsInActionOrder();
+            expect(oponentsInActionOrder).toEqual([jillBloggs]);
+        });
+    });
+
+    describe('last oponent is killed', () => {
+        beforeEach(() => {
+            sceneGetAliveActors.mockReturnValue([johnDoe]);
+            jillBloggsIsAlive.mockReturnValue(false);
+        });
+
+        it('is john doe turn', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(johnDoe);
+        });
+
+        it('is player turn after john doe', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            combatEngine.executeNextOponentAction();
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(janeDoe);
+        });
+
+        it('is john doe turn again after the player', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            combatEngine.executeNextOponentAction();
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(johnDoe);
+        });
+
+        it('updates the list of oponents in order', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const oponentsInActionOrder = combatEngine.getOponentsInActionOrder();
+            expect(oponentsInActionOrder).toEqual([johnDoe]);
+        });
+    });
+
+    describe('current oponent scapes', () => {
+        beforeEach(() => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            sceneGetAliveActors.mockReturnValue([jillBloggs]);
+            when(sceneContainsActor)
+                .mockReturnValue(true)
+                .calledWith(johnDoe)
+                .mockReturnValue(false);
+            combatEngine.executeNextOponentAction();
+        });
+
+        it('is the player turn again', () => {
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(janeDoe);
+        });
+
+        it('is the second oponent turn after the player', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const actorCurrentTurn = combatEngine.getActorCurrentTurn();
+            expect(actorCurrentTurn).toEqual(jillBloggs);
+        });
+
+        it('updates the list of oponents in order', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            const oponentsInActionOrder = combatEngine.getOponentsInActionOrder();
+            expect(oponentsInActionOrder).toEqual([jillBloggs]);
+        });
+    });
+
+    describe('getPlayerActions', () => {
+        it('throws an error if it is not the player turn', () => {
+            combatEngine.executePlayerAction(janeDoeAction);
+            try {
+                combatEngine.getPlayerActions();
+                fail('expected an error');
+            } catch (err) {}
+        });
+
+        it('returns the actions map', () => {
+            const playerActions = combatEngine.getPlayerActions();
+            expect(playerActions).toEqual(janeDoeActions);
+        });
+    });
+});
---
+++ src/engines/CombatEngine.ts
@@ -0,0 +1,118 @@
+import Action from '../actions/Action';
+import ActionBuilder from '../actions/ActionBuilder';
+import ActionsMap from '../core/ActionsMap';
+import Actor from '../core/Actor';
+import NonPlayableActor from '../core/NonPlayableActor';
+import Scene from '../core/Scene';
+
+class CombatEngine {
+    private scene: Scene;
+    private actorsTurns: Actor[];
+    private currentTurn: number;
+
+    constructor({ scene }: { scene: Scene }) {
+        this.scene = scene;
+        this.actorsTurns = [];
+        this.currentTurn = 0;
+        this.buildTurns();
+    }
+
+    private buildTurns() {
+        this.actorsTurns = [];
+        const aliveActors = this.scene.getAliveActors();
+        const player = this.scene.getPlayer()!;
+        aliveActors.forEach((oponent) => {
+            this.actorsTurns.push(player);
+            this.actorsTurns.push(oponent);
+        });
+    }
+
+    getActorCurrentTurn(): Actor {
+        return this.actorsTurns[this.currentTurn];
+    }
+
+    getOponentsInActionOrder(): NonPlayableActor[] {
+        return this.scene.getAliveActors();
+    }
+
+    executePlayerAction<T>(action: Action<T>): T {
+        const player = this.scene.getPlayer()!;
+        if (!this.isActorTurn(player)) {
+            throw new Error('Not the turn of the player!');
+        }
+        if (!action.canExecute()) {
+            throw new Error('The action cannot be executed!');
+        }
+        return this.executeAction(action);
+    }
+
+    getPlayerActions(): ActionsMap {
+        const player = this.scene.getPlayer()!;
+        if (!this.isActorTurn(player)) {
+            throw new Error('Not the turn of the player!');
+        }
+        const actionsBuilder = new ActionBuilder({
+            scene: this.scene,
+            actor: player,
+        });
+        return actionsBuilder.buildActions();
+    }
+
+    private isActorTurn(actor: Actor): boolean {
+        const actorCurrentTurn = this.getActorCurrentTurn();
+        return actor.equals(actorCurrentTurn);
+    }
+
+    private executeAction<T>(action: Action<T>): T {
+        const outcome = action.execute();
+        this.updateTurn();
+        if (this.isActorsCountChanged()) {
+            this.rebuildTurns();
+        }
+        return outcome;
+    }
+
+    private updateTurn() {
+        this.currentTurn = (this.currentTurn + 1) % this.actorsTurns.length;
+    }
+
+    private isActorsCountChanged(): boolean {
+        const aliveActors = this.scene.getAliveActors();
+        const player = this.scene.getPlayer()!;
+        const actorsInTurns = this.actorsTurns.filter(
+            (actor) => !actor.equals(player)
+        );
+        const isActorsCountChanged =
+            aliveActors.length !== actorsInTurns.length;
+        return isActorsCountChanged;
+    }
+
+    private rebuildTurns() {
+        const nextActor = this.getActorCurrentTurn();
+        const nextActorIsPlayer = nextActor.equals(this.scene.getPlayer()!);
+        const nextOponentCanAct =
+            nextActor.isAlive() && this.scene.containsActor(nextActor);
+        this.buildTurns();
+        if (nextOponentCanAct && !nextActorIsPlayer) {
+            this.currentTurn = 1;
+        } else {
+            this.currentTurn = 0;
+        }
+    }
+
+    executeNextOponentAction<T>(): [Action<T>, T] | undefined {
+        const player = this.scene.getPlayer()!;
+        if (this.isActorTurn(player)) {
+            throw new Error('Player turn!');
+        }
+        const currentOponent = this.getActorCurrentTurn() as NonPlayableActor;
+        const nextOponentAction = currentOponent.getNextAction(this.scene);
+        if (!nextOponentAction.canExecute()) {
+            return undefined;
+        }
+        const outcome = this.executeAction(nextOponentAction);
+        return [nextOponentAction, outcome];
+    }
+}
+
+export default CombatEngine;

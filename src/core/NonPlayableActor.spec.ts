113414cdfd503842e57ce33c2d804f4815a6d4fc
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-25 11:29:58 +0100

    feat: complete strategy for non playable actors

---
+++ src/core/NonPlayableActor.spec.ts
@@ -0,0 +1,137 @@
+import { when } from 'jest-when';
+import ChallengeRate from './ChallengeRate';
+import Health from './Health';
+import Inventory from './Inventory';
+import NonPlayableActor from './NonPlayableActor';
+import Scene from './Scene';
+import SkillSet from './SkillSet';
+import Weapon from './Weapon';
+import NonPlayableActorActionBuilder from '../actions/NonPlayableActorActionBuilder';
+import Damage from './Damage';
+import AttackAction from '../actions/AttackAction';
+import ReloadAction from '../actions/ReloadAction';
+import WeaponAmmunition from './WeaponAmmunition';
+import ScapeAction from '../actions/ScapeAction';
+jest.mock('../actions/NonPlayableActorActionBuilder');
+
+describe('NonPlayableActor', () => {
+    describe('getNextAction', () => {
+        let janeDoe: NonPlayableActor;
+        let scene: Scene;
+        let shotgun: Weapon;
+        let revolver: Weapon;
+        let getWeaponChallengeRate: jest.SpyInstance;
+        let buildActions: jest.SpyInstance;
+        let getAttackActions: jest.SpyInstance;
+        let getReloadActions: jest.SpyInstance;
+        let getScapeAction: jest.SpyInstance;
+        let attackWithShotgun: AttackAction;
+        let attackWithRevolver: AttackAction;
+        let reloadShotgun: ReloadAction;
+        let scapeAction: ScapeAction;
+        beforeEach(() => {
+            shotgun = new Weapon({
+                name: 'shotgun',
+                type: 'gun',
+                skill: 'aiming',
+                damage: new Damage({ min: 5, max: 8 }),
+                ammunition: new WeaponAmmunition({
+                    type: 'shells',
+                    current: 1,
+                    max: 2,
+                }),
+            });
+            revolver = new Weapon({
+                name: 'revolver',
+                type: 'gun',
+                skill: 'aiming',
+                damage: new Damage({ min: 2, max: 4 }),
+                ammunition: new WeaponAmmunition({
+                    type: 'bullets',
+                    current: 2,
+                    max: 6,
+                }),
+            });
+            getWeaponChallengeRate = jest.spyOn(
+                ChallengeRate,
+                'getWeaponChallengeRate'
+            );
+            when(getWeaponChallengeRate).calledWith(shotgun).mockReturnValue(2);
+            when(getWeaponChallengeRate)
+                .calledWith(revolver)
+                .mockReturnValue(1);
+            janeDoe = new NonPlayableActor({
+                name: 'Jane Doe',
+                health: new Health({ current: 5, max: 5 }),
+                inventory: new Inventory({ weapons: [shotgun, revolver] }),
+                skillSet: new SkillSet({}),
+            });
+            scene = {} as Scene;
+            getAttackActions = jest.fn();
+            getReloadActions = jest.fn();
+            getScapeAction = jest.fn();
+            buildActions = jest.fn().mockReturnValue({
+                getAttackActions,
+                getReloadActions,
+                getScapeAction,
+            });
+            ((NonPlayableActorActionBuilder as unknown) as jest.Mock).mockImplementation(
+                () => ({
+                    buildActions,
+                })
+            );
+
+            attackWithShotgun = new AttackAction({
+                actor: janeDoe,
+                scene,
+                oponent: janeDoe,
+                weapon: shotgun,
+            });
+            attackWithRevolver = new AttackAction({
+                actor: janeDoe,
+                scene,
+                oponent: janeDoe,
+                weapon: revolver,
+            });
+            getAttackActions.mockReturnValue([
+                attackWithShotgun,
+                attackWithRevolver,
+            ]);
+
+            reloadShotgun = new ReloadAction({
+                actor: janeDoe,
+                scene,
+                weapon: shotgun,
+            });
+            getReloadActions.mockReturnValue([reloadShotgun]);
+
+            scapeAction = new ScapeAction({ actor: janeDoe, scene });
+            getScapeAction.mockReturnValue(scapeAction);
+        });
+
+        it('returns an attack with the deadliest weapon', () => {
+            const returnedAction = janeDoe.getNextAction(scene);
+            expect(returnedAction).toEqual(attackWithShotgun);
+        });
+
+        it('they cannot attack with the deadliest weapon, return reload it', () => {
+            getAttackActions.mockReturnValue([attackWithRevolver]);
+            const returnedAction = janeDoe.getNextAction(scene);
+            expect(returnedAction).toEqual(reloadShotgun);
+        });
+
+        it('they cannot reload the deadliest weapon, return other attack', () => {
+            getAttackActions.mockReturnValue([attackWithRevolver]);
+            getReloadActions.mockReturnValue([]);
+            const returnedAction = janeDoe.getNextAction(scene);
+            expect(returnedAction).toEqual(attackWithRevolver);
+        });
+
+        it('they cannot attack nor reload, scape', () => {
+            getAttackActions.mockReturnValue([]);
+            getReloadActions.mockReturnValue([]);
+            const returnedAction = janeDoe.getNextAction(scene);
+            expect(returnedAction).toEqual(scapeAction);
+        });
+    });
+});
--- src/core/NonPlayableActor.ts
+++ src/core/NonPlayableActor.ts
@@ -1,11 +1,92 @@
+import Action from '../actions/Action';
+import AttackAction from '../actions/AttackAction';
+import NonPlayableActorActionBuilder from '../actions/NonPlayableActorActionBuilder';
+import ReloadAction from '../actions/ReloadAction';
+import ActionsMap from './ActionsMap';
 import Actor from './Actor';
-import Action from './Action';
+import ChallengeRate from './ChallengeRate';
+import Scene from './Scene';
+import Weapon from './Weapon';
 
 class NonPlayableActor extends Actor {
     private nextAction?: Action<any>;
+    private deadliestWeapon?: Weapon;
 
-    getNextAction(): Action<any> | undefined {
-        return this.nextAction;
+    getNextAction(scene: Scene): Action<any> {
+        if (!this.nextAction) {
+            this.buildNextAction(scene);
+        }
+        return this.nextAction!;
+    }
+
+    private buildNextAction(scene: Scene) {
+        const actionBuilder = new NonPlayableActorActionBuilder({
+            scene,
+            actor: this,
+        });
+        const actions = actionBuilder.buildActions();
+        if (!this.deadliestWeapon) {
+            this.deadliestWeapon = this.getDeadliestWeapon();
+        }
+        this.nextAction = this.getAttackWithDeadliestWeapon(actions);
+        if (!!this.nextAction) {
+            return;
+        }
+        this.nextAction = this.getReloadDeadliestWeapon(actions);
+        if (!!this.nextAction) {
+            return;
+        }
+        this.nextAction = this.getOtherAttack(actions);
+        if (!!this.nextAction) {
+            return;
+        }
+        this.nextAction = actions.getScapeAction();
+    }
+
+    private getDeadliestWeapon(): Weapon {
+        const weapons = this.getInventory().getWeapons();
+        return weapons.reduce((weaponWithHighestChallengeRate, weapon) => {
+            const deadliestWeaponSoFarChallengeRate = ChallengeRate.getWeaponChallengeRate(
+                weaponWithHighestChallengeRate
+            );
+            const candidateWeaponChallengeRate = ChallengeRate.getWeaponChallengeRate(
+                weapon
+            );
+            if (
+                candidateWeaponChallengeRate > deadliestWeaponSoFarChallengeRate
+            ) {
+                return weapon;
+            } else {
+                return weaponWithHighestChallengeRate;
+            }
+        }, weapons[0]);
+    }
+
+    private getAttackWithDeadliestWeapon(
+        actions: ActionsMap
+    ): AttackAction | undefined {
+        if (!this.deadliestWeapon) {
+            return;
+        }
+        return actions
+            .getAttackActions()
+            .find((action) => action.getWeapon().equals(this.deadliestWeapon!));
+    }
+
+    private getReloadDeadliestWeapon(
+        actions: ActionsMap
+    ): ReloadAction | undefined {
+        if (!this.deadliestWeapon) {
+            return;
+        }
+        return actions
+            .getReloadActions()
+            .find((action) => action.getWeapon().equals(this.deadliestWeapon!));
+    }
+
+    private getOtherAttack(actions: ActionsMap): AttackAction | undefined {
+        const attackActions = actions.getAttackActions();
+        return attackActions[0];
     }
 }
 

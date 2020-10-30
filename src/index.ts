fc80f0f17606eb7b9276ea96b38e69f45283ee3b
David Ballester Mena <david.ballester.mena@gmail.com>
2020-10-30 18:52:10 +0100

    feat: add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import NarrationsCatalogue from './narrations/NarrationsCatalogue';
+import Narration from './core/Narration';
+import NarrationEngine from './engines/NarrationEngine';
+import CombatEngine from './engines/CombatEngine';
+import Scene from './core/Scene';
+import Player from './core/Actor';
+
+export {
+    NarrationsCatalogue,
+    Narration,
+    Scene,
+    Player,
+    NarrationEngine,
+    CombatEngine,
+};
--- src/narrations/tutorial/acts/1-stage-coach/outsideTheStageCoach.ts
+++ src/narrations/tutorial/acts/1-stage-coach/outsideTheStageCoach.ts
@@ -10,6 +10,6 @@ const sceneSetupTemplate = {
         },
     },
     setup:
-        '\r\nThe sun was still up, but it was cold. It had rained the day before and the road was muddy.\r\n\r\nIn front of the stage coach, {{playerName}} could see the driver laying motionless on the mud. In front of the horses, two men advanced towards the stage coach, guns ready.\r\n\r\n"Hey, you there!" shouted one of them, taller, with a scar on his left eye.\r\n\r\n"Is it her?" asked the other, but his companion didn\'t answer. Intead, he took a new bullet from his pocket and proceeded to reload the rifle.\r\n\r\n{{playerName}} felt the thumping of her heart and could barely breath. However, she knew what was going to happen to her if she froze like that.\r\n',
+        '\nThe sun was still up, but it was cold. It had rained the day before and the road was muddy.\n\nIn front of the stage coach, {{playerName}} could see the driver laying motionless on the mud. In front of the horses, two men advanced towards the stage coach, guns ready.\n\n"Hey, you there!" shouted one of them, taller, with a scar on his left eye.\n\n"Is it her?" asked the other, but his companion didn\'t answer. Intead, he took a new bullet from his pocket and proceeded to reload the rifle.\n\n{{playerName}} felt the thumping of her heart and could barely breath. However, she knew what was going to happen to her if she froze like that.\n',
 };
 export default sceneSetupTemplate;
